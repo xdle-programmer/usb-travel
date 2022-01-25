de1c334a576e0bed4aa618cc1a69a4622492dbc7
yury-ustinov <yury.inform@gmail.com>
2022-01-25 09:17:21 +0200

    Меню админки, типы поездок

--- wp-content/themes/usb-travel/functions.php
+++ wp-content/themes/usb-travel/functions.php
@@ -13,12 +13,15 @@ if (!defined('_S_VERSION')) {
 }
 
 
-// Инициализируем добавление кастомных типов записей для лагерей
-add_action('init', 'register_camps_post_type');
+/**
+ * Лагеря
+ * Записи лагерей и управления страницами вывода
+ */
 
 function register_camps_post_type()
 {
     $labels = array(
+        'slug' => 'camp',
         'name' => _x('Лагеря', 'post type general name'),
         'singular_name' => _x('Лагерь', 'post type singular name'),
         'add_new' => 'Добавить лагерь'
@@ -28,17 +31,13 @@ function register_camps_post_type()
         'labels' => $labels,
         'description' => 'Лагеря',
         'public' => true,
-        'menu_position' => 2,
         'menu_icon' => 'dashicons-admin-multisite'
     );
-
-//    menu_icon
 
     register_post_type('camp', $args);
 }
 
-add_filter('manage_camp_posts_columns', 'camps_columns_head', 10);
-add_action('manage_camp_posts_custom_column', 'camps_columns_content', 10, 2);
+add_action('init', 'register_camps_post_type');
 
 function camps_columns_head($defaults)
 {
@@ -47,6 +46,8 @@ function camps_columns_head($defaults)
     unset($defaults['date']);
     return $defaults;
 }
+
+add_filter('manage_camp_posts_columns', 'camps_columns_head', 10);
 
 function camps_columns_content($column_name, $post_ID)
 {
@@ -60,29 +61,13 @@ function camps_columns_content($column_name, $post_ID)
     }
 }
 
-// Инициализируем добавление кастомных типов записей для стран
-add_action('init', 'register_countries_post_type');
-
-function register_countries_post_type()
-{
-    $labels = array(
-        'name' => _x('Страны', 'post type general name'),
-        'singular_name' => _x('Страна', 'post type singular name'),
-        'add_new' => 'Добавить страну'
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'description' => 'Страны',
-        'public' => true,
-    );
-
-    register_post_type('country', $args);
-}
+add_action('manage_camp_posts_custom_column', 'camps_columns_content', 10, 2);
 
 
-// Инициализируем добавление кастомных типов записей для поездок
-add_action('init', 'register_trips_post_type');
+/**
+ * Поездки.
+ * Записи поездок и управления страницами вывода
+ */
 
 function register_trips_post_type()
 {
@@ -97,15 +82,13 @@ function register_trips_post_type()
         'labels' => $labels,
         'description' => 'Поездки',
         'public' => true,
-        'menu_position' => 2,
         'menu_icon' => 'dashicons-airplane'
     );
 
     register_post_type('trip', $args);
 }
 
-add_filter('manage_trip_posts_columns', 'trips_columns_head', 10);
-add_action('manage_trip_posts_custom_column', 'trips_columns_content', 10, 2);
+add_action('init', 'register_trips_post_type');
 
 function trips_columns_head($defaults)
 {
@@ -113,6 +96,8 @@ function trips_columns_head($defaults)
     unset($defaults['date']);
     return $defaults;
 }
+
+add_filter('manage_trip_posts_columns', 'trips_columns_head', 10);
 
 function trips_columns_content($column_name, $post_ID)
 {
@@ -126,9 +111,9 @@ function trips_columns_content($column_name, $post_ID)
     }
 }
 
-//Функция замены id лагеря с любого языка на русский, для корректной привязки
-add_filter('wp_insert_post_data', 'replace_camp_id_in_trip', 10, 2);
+add_action('manage_trip_posts_custom_column', 'trips_columns_content', 10, 2);
 
+//Функция замены id лагеря с любого языка на русский, для корректной привязки
 function replace_camp_id_in_trip($data)
 {
     if ($data['post_type'] !== 'trip') {
@@ -144,12 +129,106 @@ function replace_camp_id_in_trip($data)
     return $data;
 }
 
+add_filter('wp_insert_post_data', 'replace_camp_id_in_trip', 10, 2);
 
-//// Создаем массив данных новой записи
-//    $post_data = array(
-//        'post_title' => 'Тайтл из функции внутри, ага',
-//        'post_type' => 'trip',
-//    );
-//
-//    // Вставляем запись в базу данных
-//    $post_id = wp_insert_post($post_data);
+
+/**
+ * Типы поездок.
+ * Записи поездок и управления страницами вывода
+ */
+add_action('init', 'register_trips_types_post_type');
+
+function register_trips_types_post_type()
+{
+    $labels = array(
+        'name' => _x('Типы поездок', 'post type general name'),
+        'singular_name' => _x('Тип поездки', 'post type singular name'),
+        'add_new' => 'Добавить тип поездки'
+    );
+
+    // Set various pieces of information about the post type
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Типы поездок',
+        'public' => true,
+        'menu_icon' => 'dashicons-admin-generic'
+    );
+
+    register_post_type('trip-type', $args);
+}
+
+
+/**
+ * Страны
+ * Записи стран и управления страницами вывода
+ */
+
+// Инициализируем добавление кастомных типов записей для стран
+function register_countries_post_type()
+{
+    $labels = array(
+        'name' => _x('Страны', 'post type general name'),
+        'singular_name' => _x('Страна', 'post type singular name'),
+        'add_new' => 'Добавить страну'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Страны',
+        'public' => true,
+        'menu_icon' => 'dashicons-admin-site-alt'
+    );
+
+    register_post_type('country', $args);
+}
+
+add_action('init', 'register_countries_post_type');
+
+
+/**
+ * Настройки админки.
+ * Управляем настройками меню, добавляем стили
+ */
+function wpse_custom_menu_order($menu_ord)
+{
+    if (!$menu_ord) return true;
+
+    return array(
+        'edit.php?post_type=camp', // Лагеря
+        'edit.php?post_type=page', // Страницы
+        'upload.php', // Медиа
+
+        'separator1', // Разделитель
+
+        'indigo-trips-load', // Загрузка поездок
+
+        'separator2',  // Разделитель
+        'edit.php?post_type=trip', // Поездки
+        'edit.php?post_type=trip-type', // Типы поездок
+        'edit.php?post_type=country', // Страны
+        'separator-last', // Разделитель
+
+        'link-manager.php', // Links
+        'edit-comments.php', // Comments
+        'edit.php', // Posts
+        'themes.php', // Appearance
+        'plugins.php', // Plugins
+        'users.php', // Users
+        'tools.php', // Tools
+        'options-general.php', // Settings
+        'index.php', // Dashboard
+    );
+}
+
+add_filter('custom_menu_order', 'wpse_custom_menu_order', 10, 1);
+
+add_filter('menu_order', 'wpse_custom_menu_order', 10, 1);
+
+function admin_styles()
+{
+    wp_register_style('admin-styles', get_stylesheet_directory_uri() . '/admin/style.css');
+    wp_enqueue_style('admin-styles');
+}
+
+add_action('admin_enqueue_scripts', 'admin_styles');
+
