b20fb250a58c5b7e99e125860ab6a7fd1a280641
Юрий <yury.inform@gmail.com>
2022-04-21 00:30:49 +0300

    local

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'yuryustinov_usb' );
+define( 'DB_NAME', 'usb' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'yuryustinov_usb' );
+define( 'DB_USER', 'root' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', 'WbcCFd3x' );
+define( 'DB_PASSWORD', '' );
 
 /** Имя сервера MySQL */
 define( 'DB_HOST', 'localhost' );
